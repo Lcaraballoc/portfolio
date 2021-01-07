503f08757545ac7d41e90319505ecc128e63a90f
Lcaraballoc <luisred61@gmail.com>
2021-01-06 21:41:00 -0500

    [START] Sets basic configuration for gatsby project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+export default {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "semi": "warning",
+        "react/prop-types": 0,
+        "react/display-name": 0,
+    }
+};
